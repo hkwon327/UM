de1d03a74d63a189ac39746d785b3b7ab4a7805e
hkwon327 <92026987+hkwon327@users.noreply.github.com>
2024-02-19 14:57:17 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -14,29 +14,22 @@ def get_trainer(args, return_trainer_only=True):
         mode=args.monitor.split()[0],
         filename='{epoch}-{val_loss:.2f}',
     )
-    
-    # Setup trainer arguments
-    trainer_args = {
-        "max_epochs": args.epochs,
-        "fast_dev_run": args.test_mode,
-        "num_sanity_val_steps": None if args.test_mode else 0,
-        "callbacks": [checkpoint_callback],
-        "default_root_dir": ckpt_path,
-        "deterministic": torch.cuda.is_available() and args.seed is not None,
-        "precision": 16 if args.fp16 else 32,
-    }
-    
-    # Conditionally add GPU configuration
-    if torch.cuda.is_available():
-        trainer_args["gpus"] = torch.cuda.device_count()
-
-    # Instantiate the Trainer
-    trainer = Trainer(**trainer_args)
-    
+    trainer = Trainer(
+        max_epochs=args.epochs,
+        fast_dev_run=args.test_mode,
+        num_sanity_val_steps=None if args.test_mode else 0,
+        callbacks=[checkpoint_callback],
+        default_root_dir=ckpt_path,
+        # For GPU Setup
+        deterministic=torch.cuda.is_available() and args.seed is not None,
+        gpus=torch.cuda.device_count() if torch.cuda.is_available() else None,
+        precision=16 if args.fp16 else 32,
+        # For TPU Setup
+        tpu_cores=args.tpu_cores if args.tpu_cores else None,
+    )
     if return_trainer_only:
         return trainer
     else:
         return checkpoint_callback, trainer
-
 
 
